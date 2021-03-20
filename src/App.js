b7b41db276cb41b34e40344c0a24985cf0b1cc34
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2021-03-20 23:01:51 +0530

    Chat Head

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
+import Chat from "./Components/Chat";
 import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Chat */}
+      <Chat />
     </div>
   );
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import Avatar from "@material-ui/core/Avatar";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -18,6 +26,40 @@ function Sidebar() {
           </div>
 
           <AddIcon className="sidebar__addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+
+      <div className="sidebar__profile">
+        <Avatar src="https://avatars.githubusercontent.com/u/62155169?s=460&u=de3f99d222bb29f856dbdb73fe1f3691eb414dfc&v=4" />
+        <div className="sidebar__profileInfo">
+          <h3>@vinay</h3>
+          <p>#thisIsMyID</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
         </div>
       </div>
     </div>
